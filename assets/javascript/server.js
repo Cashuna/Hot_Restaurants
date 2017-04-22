374a9e12a3a732168c14c5a58032f238588435f1
Nicole <nmarielockwood@gmail.com>
2017-04-22 11:53:51 -0400

    moving server.js

--- assets/javascript/server.js
+++ assets/javascript/server.js
@@ -3,6 +3,7 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var path = require("path");
+//var hotRestaurant = require("./assets/javascript/app.js");
 
 // Sets up the Express App
 // =============================================================
@@ -14,6 +15,81 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
+// Tables (DATA)
+// =============================================================
+var tables = [{
+  name: "Yoda",
+  phoneNumber: "555555555",
+  email: "email@gmail.com",
+  unique_ID: "2000"
+}, {
+  name: "Leia",
+  phoneNumber: "555555555",
+  email: "email@gmail.com",
+  unique_ID: "8000"
+}, {
+  name: "Darth Maul",
+  phoneNumber: "555555555",
+  email: "email@gmail.com",
+  unique_ID: "5000"
+}];
+
+var waitList = [{
+  name: "John Lennon",
+  phoneNumber: "555555555",
+  email: "email@gmail.com",
+  unique_ID: "jLen"
+}, {
+  name: "Ringo Starr",
+  phoneNumber: "555555555",
+  email: "email@gmail.com",
+  unique_ID: "ringo"
+}];
+
+
+// Routes
+// =============================================================
+
+// Basic route that sends the user to the Home Page
+app.get("/", function(req, res) {
+  //res.sendFile(path.join(__dirname, "home.html"));
+  res.send("Working");
+});
+
+// Route that sends the user to the View Tables Page
+app.get("/tables", function(req, res) {
+  res.sendFile(path.join(__dirname, "tables.html"));
+});
+
+// Route that sends the user to the Make Reservation Page
+app.get("/reserve", function(req, res) {
+  res.sendFile(path.join(__dirname, "reserve.html"));
+});
+
+// View currently reserved tables - provides JSON
+app.get("/api/tables", function(req, res) {
+  res.json(tables);
+});
+
+// View current wait list for tables - provides JSON
+app.get("/api/wait", function(req, res) {
+  res.json(waitList);
+});
+
+// Create New Characters - takes in JSON input
+app.post("/api/reserve", function(req, res) {
+  // req.body hosts is equal to the JSON post sent from the user
+  var newcharacter = req.body;
+
+  console.log(newcharacter);
+
+  // We then add the json the user sent to the character array
+  characters.push(newcharacter);
+
+  // We then display the JSON to the users
+  res.json(newcharacter);
+});
 
 // Starts the server to begin listening
 // =============================================================
