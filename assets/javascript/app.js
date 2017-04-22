393b252bddf0ab8a872462a548ac540515d3ac90
Ryan Rodwell <ryanrodwell@gmail.com>
2017-04-22 12:04:19 -0400

    updating

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -8,24 +8,24 @@ function UserReservations(name,phone,email,id){
     this.phone = phone;
     this.email = email;
     this.id = id;
-}
+};
 
 
 
 
-$("#search-btn").on("click", function() {
-    var makeResevation = $("#character-search").val().trim();
+$("#add-btn").on("click", function() {
+    var makeReservation = $("#character-search").val().trim();
 
-    makeResevation = makeResevation.replace(/\s+/g, "").toLowerCase();
+    makeReservation = makeReservation.replace(/\s+/g, "").toLowerCase();
 
     // $.get("/api/" + searchedCharacter, function(data) {
         console.log(data);
         if (data) {
             $("#stats").show();
             $("#name").text(data.name);
-            $("#role").text(data.role);
-            $("#age").text(data.age);
-            $("#force-points").text(data.forcePoints);
+            $("#phone").text(data.phone);
+            $("#email").text(data.email);
+            $("#id").text(data.id);
         }
         else {
             $("#name").text("The force is not strong with this one. Your character was not found.");
